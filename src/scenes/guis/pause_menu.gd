2fb67ab840ade4604a4a481cde15b91fd3bf0fe0
Alex Dediu <adediu25@gmail.com>
2023-12-11 16:02:47 -0500

    Finished pausing functionailty
    Pause and resume fully functional

---
+++ src/scenes/guis/pause_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+## pause menu functionality
+
+func _on_resume_pressed() -> void:
+	get_parent().resume_game()
+
+func _on_exit_pressed() -> void:
+	## add logic to reuturn to main menu
+	pass
+
+func _on_settings_pressed() -> void:
+	# add logic to bring up settings screen
+	pass 
