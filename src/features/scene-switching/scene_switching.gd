2e9b9d5d8c4f3f435e72eb0f912a6983ebf02b64
Alex Dediu <adediu25@gmail.com>
2024-01-15 16:59:29 -0500

    Add global scene switcher

---
+++ src/features/scene-switching/scene_switching.gd
@@ -0,0 +1,25 @@
+extends Node
+## Global singleton script to orchastrate scene switching
+
+## currently active scene
+var current_scene:Node = null
+
+## Sets current scene
+func _ready() -> void:
+	var root:Window = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+## Changes scene to main menu
+func goto_mainmenu() -> void:
+	goto_scene("res://scenes/guis/MainMenu.tscn")
+
+## Changes scene to scene at given path
+func goto_scene(path:String) -> void:
+	call_deferred("_deferred_go_to_scene", path)
+
+## completes a deffered scene switching
+func _deferred_go_to_scene(path:String) -> void:
+	current_scene.free()
+	var next_scene:Resource = ResourceLoader.load(path)
+	current_scene = next_scene.instantiate()
+	get_tree().root.add_child(current_scene)
