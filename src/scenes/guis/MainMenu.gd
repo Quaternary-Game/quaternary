89ed81616bdcb70632b73808f3140b07dacb5fec
Connor D. McKinney <cdmckinney1001@gmail.com>
2024-01-16 19:03:08 -0500

    Finalized preliminary main menu
    Placeholders for scene switching in place, just need to actually switch scenes

---
+++ src/scenes/guis/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_mini_game_button_pressed() -> void:
+	pass # Replace with scene switch to mini game menu scene
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
--- src/scenes/guis/mini_game_menu.gd
+++ src/scenes/guis/mini_game_menu.gd
@@ -5,3 +5,13 @@ func _ready() -> void:
 	$play_button.disabled = true
 
 
+func _on_pressed() -> void:
+	$play_button.disabled = false
+
+func _on_play_button_pressed() -> void:
+	var mini_game: String = $mini_game_scroller/mini_game_box/build_the_codons.button_group.get_pressed_button().text
+	print(mini_game)
+	#Add scene switch to appropriate mini game
+
+func _on_main_menu_button_pressed() -> void:
+	pass # Replace with scene switch to main
