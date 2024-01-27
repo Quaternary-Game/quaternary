63734a4a6cc0a193dad5dbe63e3c9fae047e8a44
David Lewis <davidalewis00@gmail.com>
2024-01-27 17:32:57 -0500

    Visual update to mutation minigame

--- src/features/dynamicmenu/DragButton.gd
+++ src/features/dynamicmenu/DragButton.gd
@@ -2,7 +2,7 @@ class_name DragButton extends Button
 ## Button that can be dragged.
 ##
 ## Meant to be used in combination with Dynamic Menu, or any class that implements
-## [method DynamicMenu.hide_slide_child] and [method DyanamicMenu.reveal_slide_child] 
+## [method DynamicMenu.hide_slide_child] and [method DyanamicMenu.reveal_slide_child]
 
 ## set this in child classes to control the drop data of the button, can be anything
 var drop_data: Variant
@@ -14,12 +14,12 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	var me: DragButton = self.duplicate()
 	preview.add_child(me)
 	me.position = Vector2(0,0)
-	
+
 	get_parent().hide_slide_child(self)
 	set_drag_preview(preview)
 	dragging = true
 	return drop_data
-	
+
 var done: bool = false
 
 func _notification(what: int) -> void:
@@ -31,5 +31,5 @@ func _notification(what: int) -> void:
 			if is_drag_successful() and not self.visible:
 				done = true
 
-		
+
 
