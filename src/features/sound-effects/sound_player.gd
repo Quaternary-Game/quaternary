40505a52cd61b39dc24263247e9b8b11838bcf20
Alex Dediu <adediu25@gmail.com>
2024-03-29 16:45:21 -0400

    sound on button hover

--- src/features/sound-effects/sound_player.gd
+++ src/features/sound-effects/sound_player.gd
@@ -3,7 +3,8 @@ extends Node
 
 ## AudioStream object for all sound effects
 var streams:Dictionary = {
-	'select': preload("res://features/sound-effects/assets/select.mp3")
+	'select': preload("res://features/sound-effects/assets/select.mp3"),
+	'hover': preload("res://features/sound-effects/assets/hover.mp3")
 }
 
 #@onready var player:AudioStreamPlayback
@@ -18,23 +19,36 @@ func _ready() -> void:
 		players[sound] = AudioStreamPlayer.new()
 		add_child(players[sound])
 		players[sound].stream = streams[sound]
-		
+	
+	players.select.volume_db = -12
+	players.hover.volume_db = -12
+	
 	# this makes sure we connect buttons that 
 	# in the main menu
 	connect_buttons(get_tree().root)
 	
 	# connect signal for nodes added
 	get_tree().node_added.connect(_on_node_added)
+	
+	
 
+# recursive function for initial signal connection
 func connect_buttons(root:Node) -> void:
 	for child in root.get_children():
 		if child is Button:
 			child.pressed.connect(play_select)
+			child.mouse_entered.connect(play_hover)
 		connect_buttons(child)
-		
+
+# connect button press signal when node added to tree
 func _on_node_added(node:Node) -> void:
 	if node is Button:
 		node.pressed.connect(play_select)
+		node.mouse_entered.connect(play_hover)
 
+# play sound functions
 func play_select() -> void:
 	players.select.play(0.25)
+
+func play_hover() -> void:
+	players.hover.play(0.15)
