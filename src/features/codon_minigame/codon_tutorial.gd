55565ea0293eeadb90d6b76298b0a74fda24b2f2
Connor D. McKinney <cdmckinney1001@gmail.com>
2024-04-04 10:33:03 -0400

    Target Highlighting now
    It works

--- src/features/codon_minigame/codon_tutorial.gd
+++ src/features/codon_minigame/codon_tutorial.gd
@@ -15,29 +15,30 @@ func _on_pressed() -> void:
 	lbl.autowrap_mode = TextServer.AUTOWRAP_WORD
 	lbl.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 	lbl.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	tut = true
 	var sbox := StyleBoxFlat.new()
 	lbl.add_theme_font_size_override("font_size", 32)
 	lbl.set_offsets_preset(Control.PRESET_TOP_LEFT)
 	lbl.set_anchors_preset(Control.PRESET_CENTER_TOP)
-	sbox.border_width_left = 10
-	sbox.border_width_bottom = 10
-	sbox.border_width_right = 10
-	sbox.border_width_top = 10
+	sbox.set_border_width_all(13)
 	sbox.border_blend = true
+	sbox.bg_color = Color(0, 0, 0, .4)
 	sbox.set_corner_radius_all(7)
-	#sbox.shadow_size = 5
+
 	
 
-	#sbox.set_border_width_all(200)
 	sbox.border_color = Color("8fbcbb")
-	sbox.draw_center = false
 	lbl.add_theme_stylebox_override("normal", sbox)
 	var path: String = get_parent().get_path()
-	var pos: Object = get_node(path + "/StartButton")
+	var target: Object = get_node(path + "/StartButton")
 	lbl.size = Vector2(400, 100)
-	#lbl.position = Vector2(0, 0)
-	get_parent().add_child(lbl) # Add it as a child of this node.
-	#print(get_parent())
-	#lbl.position = ((get_global_mouse_position() / 2) + Vector2(25, 50))
+	target.add_child(lbl) # Add it as a child of this node.
+	lbl.position = lbl.position + Vector2(-400, -400)
+	
+	var tsBox: = StyleBoxFlat.new()
+	tsBox.set_border_width_all(13)
+	tsBox.border_blend = true
+	tsBox.bg_color = Color("4c566a")
+	tsBox.border_color = Color(1, .9, .07, 1)
+	tsBox.set_corner_radius_all(7)
+	target.add_theme_stylebox_override("normal", tsBox)
 	#get_tree().paused = true
