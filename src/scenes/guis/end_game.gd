f3ce86baa83cd879090ed9a7a958c7a611864fc6
Alex Dediu <adediu25@gmail.com>
2024-04-08 23:36:27 -0400

    fix broken screen from colorblind shader

---
+++ src/scenes/guis/end_game.gd
@@ -0,0 +1,9 @@
+extends Control
+
+var parent:Node
+
+func _on_main_menu_button_pressed() -> void:
+	SceneSwitching.goto_mainmenu()
+
+func _on_restart_button_pressed() -> void:
+	SceneSwitching.goto_scene(SceneSwitching.current_scene.scene_file_path)
