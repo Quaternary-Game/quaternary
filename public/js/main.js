8a848568fa62cda5c6ec3345ac84ccc6600f254d
IllustratedMan-code <davidalewis00@gmail.com>
2024-08-01 14:33:09 -0400

    initial hugo site

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+(() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", function() {
+    const title = document.querySelector(".rainbow-title");
+    title.innerHTML = title.textContent.split("").map(
+      (letter) => `<span>${letter}</span>`
+    ).join("");
+  });
+})();
